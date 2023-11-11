a715db8b840e18454403542081082bb9b0b502a7
JimF04 <133442205+JimF04@users.noreply.github.com>
2023-11-10 19:11:55 -0600

    Arreglo de registro y getAllUsers

--- src/main/java/com/apiproyect/NotUberServer/Controller/UserController.java
+++ src/main/java/com/apiproyect/NotUberServer/Controller/UserController.java
@@ -4,9 +4,9 @@ import com.apiproyect.NotUberServer.Model.Driver;
 import com.apiproyect.NotUberServer.Model.Employee;
 import com.apiproyect.NotUberServer.XMLHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +30,18 @@ public class UserController {
     public List<Employee> getAllEmployees() {
         // Lógica para obtener todos los empleados del XML
         return xmlHandler.getAllUsers("employee", Employee.class);
+    }
+
+    @PostMapping("/register/driver")
+    public ResponseEntity registerNewDriver(@RequestBody Driver driver) {
+        xmlHandler.registerUser("driver", driver);
+        return new ResponseEntity<>("Driver registered successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/register/employee")
+    public ResponseEntity registerNewEmployee(@RequestBody Employee employee) {
+        xmlHandler.registerUser("employee", employee);
+        return new ResponseEntity<>("Employee registered successfully", HttpStatus.OK);
     }
 }
 
--- src/main/java/com/apiproyect/NotUberServer/XMLHandler.java
+++ src/main/java/com/apiproyect/NotUberServer/XMLHandler.java
@@ -8,13 +8,11 @@ import org.jdom2.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -61,25 +59,90 @@ public class XMLHandler {
                 .collect(Collectors.toList());
     }
 
-    // Método auxiliar para convertir un elemento a un objeto Java
+    // Método auxiliar para convertir un elemento a un objeto Java utilizando solo JDOM
     private <T> T convertElementToObject(Element element, Class<T> clazz) {
         try {
-            // Convierte el Element a un String XML
-            String xmlString = new XMLOutputter().outputString(element);
+            T instance = clazz.getDeclaredConstructor().newInstance();
 
-            // Configura el contexto JAXB para la clase especificada
-            JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+            // Obtén todos los campos de la clase del objeto
+            Field[] fields = clazz.getDeclaredFields();
 
-            // Crea el unmarshaller
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            // Itera sobre los campos y establece los valores desde el elemento XML
+            for (Field field : fields) {
+                field.setAccessible(true);
+                String fieldName = field.getName();
 
-            // Deserializa el String XML al objeto Java
-            StringReader reader = new StringReader(xmlString);
-            return (T) unmarshaller.unmarshal(reader);
-        } catch (JAXBException e) {
+                // Busca el elemento correspondiente al campo en el XML
+                Element fieldElement = element.getChild(fieldName);
+
+                if (fieldElement != null) {
+                    // Obtiene el valor del elemento y establece el campo en el objeto
+                    String fieldValue = fieldElement.getText();
+                    field.set(instance, convertStringToFieldType(fieldValue, field.getType()));
+                }
+            }
+
+            return instance;
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    // Método auxiliar para convertir un String a un tipo específico de campo
+    private Object convertStringToFieldType(String stringValue, Class<?> fieldType) {
+        if (fieldType == String.class) {
+            return stringValue;
+        } else if (fieldType == Integer.class || fieldType == int.class) {
+            return Integer.parseInt(stringValue);
+        } else if (fieldType == Double.class || fieldType == double.class) {
+            return Double.parseDouble(stringValue);
+        } else if (fieldType == Long.class || fieldType == long.class) {
+            return Long.parseLong(stringValue);
+        } else if (fieldType == Boolean.class || fieldType == boolean.class) {
+            return Boolean.parseBoolean(stringValue);
+        }
+        return stringValue;
+    }
+
+    public void registerUser(String elementType, Object user) {
+        Document document = readXML(filePath);
+        Element rootElement = document.getRootElement();
+
+        // Crea un nuevo elemento para el usuario
+        Element newUserElement = convertObjectToElement(user, elementType);
+
+        // Agrega el nuevo elemento al elemento raíz
+        rootElement.addContent(newUserElement);
+
+        // Guarda los cambios en el archivo XML
+        saveXML(document, filePath);
+    }
+
+    private Element convertObjectToElement(Object object, String elementType) {
+        Element element = new Element(elementType);
+
+        // Obtén todos los campos de la clase del objeto
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        // Itera sobre los campos y agrega elementos al XML
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                String fieldName = field.getName();
+                Object fieldValue = field.get(object);
+
+                // Crea un nuevo elemento para cada campo y agrega al elemento principal
+                Element fieldElement = new Element(fieldName);
+                fieldElement.setText(String.valueOf(fieldValue));
+                element.addContent(fieldElement);
+
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return element;
     }
 
     private static void saveXML(Document document, String filePath) {
