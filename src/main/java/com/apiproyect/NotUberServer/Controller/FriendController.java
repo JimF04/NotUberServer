a150b830de9a8f23130a30f85b377b7b2f9b2fbf
Samuel Mar?n Soto <samarin@estudiantec.cr>
2023-11-17 00:17:03 -0600

    agregar metodos addFriend y getFriends a FriendController para darle a servidor habilidad de responder a requests que le pidan agregar un amigo y obtener una lista de amigos

--- src/main/java/com/apiproyect/NotUberServer/Controller/FriendController.java
+++ src/main/java/com/apiproyect/NotUberServer/Controller/FriendController.java
@@ -7,17 +7,14 @@ import com.apiproyect.NotUberServer.XMLHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 
 /**
- * Controlador que gestiona las operaciones de inicio de sesión para conductores y empleados.
+ * Controlador que gestiona las operaciones de amigos para conductores y empleados.
  */
 @RestController
 @RequestMapping("/api")
@@ -27,50 +24,16 @@ public class FriendController {
     XMLHandler xmlHandler;
 
     @PostMapping("/addfriend")
-    public ResponseEntity getFriends(@RequestBody String email) {
-
-        if (friend.isEmpty()){
-            return ResponseEntity.badRequest().body("Empty friend field");
+    public ResponseEntity addFriend(@RequestBody String friendname, String email) {
+        if (friendname.isEmpty()) {
+            return new ResponseEntity<>("Friend field must not be empty", HttpStatus.BAD_REQUEST);
         }
-
-        // Obtener todos los conductores del XML
-        String  drivers = xmlHandler.addfriend("driver", Driver.class);
-
+        xmlHandler.addFriend(friendname, email);
+        return new ResponseEntity<>("friend added successfully", HttpStatus.OK);
     }
 
-    /**
-     * Procesa la solicitud de inicio de sesión para empleados.
-     *
-     * @param logIn Objeto LogIn que contiene la información de inicio de sesión del empleado.
-     * @return ResponseEntity con un mensaje indicando el resultado del inicio de sesión y el código de estado HTTP correspondiente.
-     */
-    @PostMapping("/friends")
-    public ResponseEntity logInEmployee(@RequestBody LogIn logIn) {
-        String email = logIn.getEmail();
-        String password = logIn.getPassword();
-
-        if (email.isEmpty() || password.isEmpty()) {
-            return ResponseEntity.badRequest().body("Please complete all fields");
-        }
-
-        // Obtener todos los empleados del XML
-        List<Employee> employees = xmlHandler.getAllUsers("employee", Employee.class);
-
-        // Buscar el empleado con el correo proporcionado
-        Optional<Employee> employeeOptional = employees.stream()
-                .filter(employee -> employee.getEmail().equals(email))
-                .findFirst();
-
-        if (employeeOptional.isPresent()) {
-            // Verificar la contraseña
-            Employee employee = employeeOptional.get();
-            if (employee.getPassword().equals(password)) {
-                return new ResponseEntity<>("Employee logged in successfully", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Incorrect password", HttpStatus.UNAUTHORIZED);
-            }
-        } else {
-            return new ResponseEntity<>("Employee not found", HttpStatus.UNAUTHORIZED);
-        }
+    @GetMapping("/friends")
+    public String getFriends(@RequestBody String email) {
+        return xmlHandler.getFriends(email);
     }
 }
